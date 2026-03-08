b584d881ab120b6d52c55c088f8dc86ff759482f
ducbeo <ducntse03282@gmail.com>
2026-03-08 15:40:02 +0700

    h

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/public/header";
+import Footer from "@/components/public/footer";
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="min-h-screen">
+            <Header />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
